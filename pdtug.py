d5c8d62799970f70f669c6eeedc65e5dbd8cd2cd
caiocarneloz <caiocarneloz@gmail.com>
2019-05-04 18:30:52 -0300

    fix_dist

--- pdtug.py
+++ pdtug.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import numpy as np
-import sys
 
 def pdtug(df, k):
 
     graph = {}
-    dist_df = pd.DataFrame(pd.np.empty((len(df), 0)) * pd.np.nan)
-
+    
     for i in range(0,len(df)):
 
-        dist_df['dist'+str(i)] = sys.float_info.max
+        dist = [float("inf")] * len(df)
 
         actual = df.values[i]
 
         for j in range(0,len(df)):
             if(i != j):
-                dist_df['dist'+str(i)].values[j] = np.linalg.norm(actual-df.values[j])
+                dist[j] = np.linalg.norm(actual-df.values[j])
 
-        dist_df = dist_df.sort_values('dist'+str(i))
-        graph[i] = list(dist_df['dist'+str(i)].index[0:k])
+        dist = np.array(dist)
+        dist = np.argsort(dist)
+        graph[i] = list(dist[0:k])
 
     for i in range(0,len(df)):
         graph[i] += ([k for k,v in graph.items() if i in v])
